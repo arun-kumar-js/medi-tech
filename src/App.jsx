5e05dc08a41d2b11d0e5f9476e86fcea531384f8
Hemapriya182024 <hemakarthikeyan2024@gmail.com>
2024-12-03 13:09:13 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Certification from './Components/Certification';
 function App() {
   return (
     <>
-    <div className="overflow-x-hidden text-stone-300 antialiased">
+    <div className="overflow-x-hidden text-stone-300  antialiased">
       <div className='fixed inset-0 -z-10'>
         <div className="relative size-full bg-black">
           <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
